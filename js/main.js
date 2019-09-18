3d2521546a70ff19ba98eaf42d5e818f2fa7921d
Huyen Phan <huyenpt905@gmail.com>
2019-09-18 18:06:15 +0700

    update header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+
+const header = document.getElementById('header');
+const topBar = document.querySelector('ul.topbar');
+const mobileNav = document.querySelector('.mobile-nav');
+const iconMobileNav = document.querySelector('.mobile-nav i');
+const mainNav =  document.querySelector('.main-nav');
+const listNav = document.querySelector('.main-nav .main-nav__list');
+
+window.addEventListener('load', () => {
+    document.querySelector('.preloader').style.display = 'none';
+});
 document.addEventListener('DOMContentLoaded', () => {
-    const header = document.getElementById('header');
-    const topBar = document.querySelector('ul.topbar');
     let status = '1';
     window.addEventListener('scroll', () => {
         if(window.pageYOffset > 100) {
@@ -8,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 header.classList.add('bg-while');
                 topBar.classList.add('d-none');
                 status = '0';
+                console.log('he');
             }
         }
         else if(window.pageYOffset <= 100) {
@@ -18,4 +28,22 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+
+    mobileNav.addEventListener('click', () => {
+       showMobileNav();
+
+    });
+
+
 });
+
+function showMobileNav() {
+    iconMobileNav.classList.toggle('fa-bars');
+    iconMobileNav.classList.toggle('fa-times');
+    iconMobileNav.classList.toggle('c-white');
+    mainNav.classList.toggle('bg-purple');
+    listNav.classList.toggle('show-nav');
+}
+function hideMobileNav() {
+
+}
